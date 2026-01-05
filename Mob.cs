ece065ed3104a5f5587253fac15d9bde7fb9b796
Michael Taylor <241386356+ElephantInTheRoom-Michael@users.noreply.github.com>
2026-01-04 23:24:08 -0800

    Complete step 4 of tutorial - Designing the mob scene

---
+++ Mob.cs
@@ -0,0 +1,36 @@
+using Godot;
+
+namespace SquashTheCreeps;
+
+public partial class Mob : CharacterBody3D
+{
+    [Export(PropertyHint.None, "suffix:m/s")]
+    public int MinSpeed { get; set; } = 10;
+    
+    [Export(PropertyHint.None, "suffix:m/s")]
+    public int MaxSpeed { get; set; } = 18;
+
+    public override void _PhysicsProcess(double delta)
+    {
+        MoveAndSlide();
+    }
+    
+    public void Initialize(Vector3 startPosition, Vector3 playerPosition)
+    {
+        // We position the mob by placing it at startPosition
+        // and rotate it towards playerPosition, so it looks at the player.
+        LookAtFromPosition(startPosition, playerPosition, Vector3.Up);
+        // Rotate this mob randomly within range of -45 and +45 degrees,
+        // so that it doesn't move directly towards the player.
+        RotateY(GD.RandRange(-Mathf.Pi / 4.0, Mathf.Pi / 4.0));
+        
+        var randomSpeed = GD.RandRange(MinSpeed, MaxSpeed);
+        Velocity = Vector3.Forward * randomSpeed;
+        Velocity = Velocity.Rotated(Vector3.Up, Rotation.Y);
+    }
+    
+    private void OnScreenExited()
+    {
+        QueueFree();
+    }
+}
